d9b726cda5acae1508c8c74c570c65f9cdf61e87
Shane Winterhalter <shanetwinterhalter@gmail.com>
2026-05-11 08:49:20 +0100

    Update nvim explorer hotkeys and behaviour

---
+++ nvim/lua/plugins/snacks-explorer.lua
@@ -0,0 +1,34 @@
+return {
+  "folke/snacks.nvim",
+  keys = {
+    { "<leader>e", "<leader>fE", desc = "Explorer (cwd)", remap = true },
+    { "<leader>E", "<leader>fe", desc = "Explorer (root dir)", remap = true },
+  },
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          actions = {
+            cd_up = function(picker)
+              local parent = vim.fs.dirname(picker:cwd())
+              vim.fn.chdir(parent)
+              picker:set_cwd(parent)
+              picker:find()
+            end,
+          },
+          win = {
+            input = {
+              focusable = false,
+            },
+            list = {
+              keys = {
+                ["<c-d>"] = "cd",
+                ["<c-u>"] = "cd_up",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
